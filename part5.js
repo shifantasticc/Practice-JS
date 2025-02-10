1e184c7b199d668fb76045e3bd184ceeaa2e4316
shifantasticc <shifastudyit@gmail.com>
2025-02-11 01:25:26 +0530

    JS-Practice Part-5

---
+++ part5.js
@@ -0,0 +1,73 @@
+// let student = {
+//     name: "Shifa",
+//     age: 19,
+//     marks: 76,
+//     city: "mumbai"
+// };
+
+// //CREATE A THREAD/X POST!!
+// const post ={
+//     username: "@shifantastic",
+//     content: "my 1st post",
+//     likes: 150,
+//     reposts:5,
+//     tags: ["@zoya", "@noorin"]
+// };
+
+//OBJECTS OF OBJECTS
+// const classInfo = {
+//     shifa: {
+//         grade: "A",
+//         city: "mumbai"
+//     },
+//     zoya: {
+//         grade: "A+",
+//         city: "Navi Mumbai"
+//     },
+//     fareen: {
+//         grade: "O",
+//         city: "jharkhand"
+//     }
+// };
+
+//ARRAY OF OBJECTS
+// const classInfo = [
+//     {
+//         name: "shifa",
+//         grade: "A",
+//         city: "mumbai"
+//     },
+//     {
+//         name: "zoya",
+//         grade: "A+",
+//         city: "Navi Mumbai"
+//     },
+//     {
+//         name: "fareen",
+//         grade: "O",
+//         city: "jharkhand"
+//     }
+// ];
+
+// Q.1 GENERATE A RANDOM NUUMBER LIKE A DICE ROLL [1 TO 6]
+// let dice = Math.floor(Math.random()*6) +1;
+// console.log(dice);
+
+// Q.2 CREATE AN OBJ CAR WITH FOLL PROPERTIES
+// const car = {
+//     name: "Mercedes Benz",
+//     model: "S-Class",
+//     color: "Black",
+// };
+// console.log(car.name);
+
+// Q.3 CREATE AN OBJ PERSON AND DO FOLL CAHNGES ON IT
+// let person = {
+//     name: "Shifa",
+//     age: 19,
+//     city: "mumbai",
+// };
+
+// person.city = "New York";
+// person.country = "United States";
+// console.log(person);
