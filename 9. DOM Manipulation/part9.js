5bf2646f1cc33b328568d10a9eef079e24316a81
shifantasticc <shifastudyit@gmail.com>
2025-02-17 15:21:21 +0530

    8. Array methods

---
+++ 9. DOM Manipulation/part9.js
@@ -0,0 +1,49 @@
+// let para1 = document.createElement('p');
+//  para1.innerText = "Hey im red!";
+//  document.querySelector('body').append(para1);
+
+//  para1.classList.add("red");
+
+//  let h3 = document.createElement('h3');
+//  h3.innerText = "Hey im blue h3";
+//  document.querySelector('body').append(h3);
+
+//  h3.classList.add("blue");
+
+//  let div = document.createElement("div");
+//  let h1 = document.createElement("h1");
+//  let para2 = document.createElement("para2");
+
+//  h1.innerText = "I'm in a div";
+//  para2.innerText = "MEE TOO!";
+
+//  div.append(h1);
+//  div.append(para2);
+//  div.classList.add("box");
+//  document.querySelector('body').append(div);
+
+//  //Q.1
+//  let btn = document.createElement("button");
+//  let input = document.createElement("input");
+//  btn.innerText = "Click Me!";
+
+//  document.querySelector('body').append(input);
+//  document.querySelector('body').append(btn);
+
+//  //Q.2 
+//  btn.setAttribute("id", "button");
+//  input.setAttribute("placeholder", "username");
+
+// //Q.3
+//  let btn = document.querySelector(btn);
+//  btn.classList.add("btnStyle");
+
+//  //Q.4 
+//  let h1 = document.createElement("h1");
+//  h1.innerHTML= "<u>DOM Practice</u>"; 
+//  document.querySelector("body").append(h1);
+
+//  //Q.5 
+//  let p = document.createElement("p");
+//  p.innerHTML="Apna College <b>Delta</b> Practice";
+//  document.querySelector("body").append(p);
